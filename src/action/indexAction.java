55b8468a7f31be7ac7d159e43d755c9890cf1f5c
Fecan2012 <fecan2012@gmail.com>
2016-08-06 20:46:40 -0700

    Add files via upload

--- src/action/indexAction.java
+++ src/action/indexAction.java
@@ -7,6 +7,13 @@ public class indexAction implements CommandAction {
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		if (request.getParameter("sectl") != null) {
+			int sectl = Integer.parseInt(request.getParameter("sectl"));
+			if (sectl == 1) {
+				request.getSession().removeAttribute("mem_id");
+				request.getSession().removeAttribute("grade");
+			}
+		}
 		return "/index.jsp";
 	}
 }
